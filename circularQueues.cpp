52e9bd6137a0c30554d3ea10ec759a3b957d4a93
naufaalkh <naufaalkh@gmail.com>
2024-05-30 18:31:26 +0700

    iterasi dari awal array hingga near

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -99,5 +99,14 @@ public:
             }
             cout << endl;
         }
+
+        FRONT_Position = 0;
+
+        // Iterasi dari awal array hingga REAR
+        while (FRONT_Position <= REAR_Position)
+        {
+            cout << queue_array[FRONT_Position] << " ";
+            FRONT_Position++;
+        }
     }
 };
