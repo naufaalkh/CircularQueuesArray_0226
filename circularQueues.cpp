c71c6137e284b86db73bbdbef19d536ebd304bd3
naufaalkh <naufaalkh@gmail.com>
2024-05-30 18:21:46 +0700

    membuat inputan

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -6,4 +6,12 @@ private:
     int FRONT, REAR, max = 5;
     int queue_array[5];
     // menetapkan antrian masih kosong
+
+public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+    // membuat inputan
 };
